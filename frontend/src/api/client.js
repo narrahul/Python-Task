78219c11e3b5cd0804481cfd15efaabb1077114d
rahulnar10 <narrahul10@gmail.com>
2025-11-04 14:38:09 +0530

    first commit

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+const API_BASE = (import.meta.env.VITE_API_BASE_URL || "") + "/api";
+
 const client = axios.create({
-  baseURL: "/api",
+  baseURL: API_BASE,
   headers: {
     "Content-Type": "application/json"
   }
